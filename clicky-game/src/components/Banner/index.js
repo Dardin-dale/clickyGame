b4bf2539f877803885f35f1fbf485ac00152ec0b
Dardin <logandalec@gmail.com>
2019-01-09 00:00:34 -0800

    done

--- clicky-game/src/components/Banner/index.js
+++ clicky-game/src/components/Banner/index.js
@@ -3,11 +3,21 @@ import "./style.css";
 
 function Banner (props) {
     return (
-        <ul>
-          <li>Clicky Game!</li>  
-          <li>Score: {props.score} | </li>
-          <li>TopScore: {props.topScore}</li>
-        </ul>
+        <div>
+          <nav className="navbar fixed-top">
+            <ul>
+                <li className="navbar-brand nav-item">Clicky Game!</li>  
+                <li className="nav-item">Click an image to begin</li>
+                <li className="nav-item">Score: {props.score} | TopScore: {props.topScore}</li>
+            </ul>
+          </nav>
+
+          <header className="jumbotron">
+            <h1>Clicky Game!</h1>
+            <h2>Click on an image to earn points! Don't click on the same image twice!</h2>
+          </header>
+
+        </div>
     );
 }
 
