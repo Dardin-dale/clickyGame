aaaf9f9e139edf318db165c30e418aea686fe385
Dardin <logandalec@gmail.com>
2019-01-09 23:21:23 -0800

    final

---
+++ build/precache-manifest.3f47188d8437c7aa684dd8922954139e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8960ba07a94cffee45bf3bef940d3f7",
+    "url": "/clickyGame/static/media/pink_dust.b8960ba0.png"
+  },
+  {
+    "revision": "f5e1124212e913e930db",
+    "url": "/clickyGame/static/js/runtime~main.f5e11242.js"
+  },
+  {
+    "revision": "e224162dd01e7912af89",
+    "url": "/clickyGame/static/js/main.e224162d.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clickyGame/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "e224162dd01e7912af89",
+    "url": "/clickyGame/static/css/main.07247e88.chunk.css"
+  },
+  {
+    "revision": "f0a01263ffaae8db996dccd2ac2d8b98",
+    "url": "/clickyGame/index.html"
+  }
+];
